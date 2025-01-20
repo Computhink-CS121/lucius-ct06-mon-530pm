7611ecae5d8247c441193a6c37c2eeaf9b0e88b9
Computhink-CS121 <cs121@computhink.com.sg>
2025-01-20 10:44:34 +0000

    Jan 20, 2025, 10:44 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,10 +1,20 @@
+
 print(myage=12)
 
+# task 1B
+print(20 )
 
 
+variable1 = 3
+variable1 = 5
+5 + 3 = 8
 
+# create variable(a)
+a = 9
+# create variable(b)
+b = 88
+print(a+b)
 
-
-
+# print (subtract 10 from 50 then mutipl
 
 
