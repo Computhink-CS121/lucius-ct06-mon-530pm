feea6a002e011d5aa9ad231460f4cfbe2221b732
Computhink-CS121 <cs121@computhink.com.sg>
2025-01-20 09:51:04 +0000

    Jan 20, 2025, 9:51 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -1,7 +1,73 @@
 print("Hello from lesson 2")
 
 ######## Write the pseudocode in comments for task 2 here
-# Using comments, translate the code shown on screen into pseudocode.
+# Using comments, translate the code shown on screen int
+#repeat 10 times
+#say hey!
+#move 10 steps
 
-######## Write the pseudocode in comments for task 3 here
-# Using comments, translate the code shown on screen into pseudocode.
+#set counter to 0
+#repeat until counter 50
+#move 10 steps
+#move 10 steps
+#turn 15 degrees
+#increase "counter" by 10
+
+#ask what is your age and wait
+#if answer is less 18
+#say access denied
+#if else say welcome
+
+#look at the material
+#if material is glass
+#put glass into glass bin
+#if material is plastic
+#put in plastic bin
+#if material is paper
+#then put it in the paper bin
+
+#if user type word other than eel rice
+#then show error message
+#if user thype eel rice then congratulate
+#get the 
+#give user 4 tests
+#get user test 1 score 
+#get user  2 test score
+#get user score for 3 test
+#get user test 4 score
+#take 20 percent of test 1
+#take 40 percent of test 2
+#take 40 percent of test 3
+#add up the 3 numbers, and print the final score
+
+test1 = input("what is your test score")
+test1 = int(test1)# conversion function
+test2 = input("what is your test score")
+test2 = int(test1)# conversion function
+
+test3 = input("what is your test score")
+test3 = int(test1)# conversion function
+
+test4 = 88
+
+test1 = 0.25* test1 #25%test1 = input("what is your test score")
+test1 = int(test1)# conversion function
+test2 = 0.25* test2 #25%
+test3 = 0.25* test3 #25%
+test4 = 0.25* test4 #25%
+
+final = test1 + test2 + test3 + test4
+
+print("your final score is",final)
+
+
+ 
+
+
+
+
+
+
+
+
+
