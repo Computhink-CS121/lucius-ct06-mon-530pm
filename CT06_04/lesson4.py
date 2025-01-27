3fe54430a07a0ff5fdd0bd6f7edf8665157c1f6f
Computhink-CS121 <cs121@computhink.com.sg>
2025-01-27 10:07:11 +0000

    Jan 27, 2025, 10:07 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,10 +1,4 @@
-print("Hello from lesson 4")
-reply = input("what is your name?")
-print("hello" + reply)
-# reply = 100
-counter = 500
-print(counter * reply)
-
+print("Hello from lesson 4")33
 
 
 
