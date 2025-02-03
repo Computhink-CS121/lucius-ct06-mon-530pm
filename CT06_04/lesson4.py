926fbd553a5ea795bd5b2d68655833598d7af7a9
Computhink-CS121 <cs121@computhink.com.sg>
2025-02-03 09:46:23 +0000

    Feb 3, 2025, 9:46 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -16,7 +16,7 @@
 
 #Assign a name to the variable "user_name"
 #Assign an integer to the variable "user_age"
-#Use type conversion and string concatenation to print
+#Use type conversion and string concatenation to print out
 
 # year_born = input("what year are you born")
 # year_born = int(year_born)
