ae50d4fcc69d408e70cad35bc1f89d355174cef1
Computhink-CS121 <cs121@computhink.com.sg>
2025-02-03 09:37:10 +0000

    Feb 3, 2025, 9:37 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,25 +1,25 @@
-print("Hello from lesson 4")
-name = input("what is your name? ")
-print("Hey, "+ name)
-print("what hobby do you like ")
-print("i enjoy ("hobby too" + nobody)
-print(Total Cost: 17.95 dollars " +" 1* extra large curry chicken pizza)
-num1 = 18
-num2 = 50
-ans = num1 + num2
+# print("Hello from lesson 4")
+# name = input("what is your name? ")
+# print("Hey, "+ name)
+# print("what hobby do you like ")
+# print("i enjoy ("hobby too" + nobody)
+# print(Total Cost: 17.95 dollars " +" 1* extra large curry chicken pizza)
+# num1 = 18
+# num2 = 50
+# ans = num1 + num2
 
-print(str(num1) + " + " + str(num2) + " = " + str(ans)
-age = input("what is your age? ")
-print("Your age next year is: " + str(int(age) + 1))
-
-
+# print(str(num1) + " + " + str(num2) + " = " + str(ans)
+#age = input("what is your age? ")
+#print("Your age next year is: " + str(int(age) + 1))
 
 
 
-year_born = input("what year are you born")
-year_born = int(year_born)
-current_year = 2025
-age = currect year = 2025
+
+
+# year_born = input("what year are you born")
+# year_born = int(year_born)
+# current_year = 2025
+# age = currect year = 2025
 
 
 
