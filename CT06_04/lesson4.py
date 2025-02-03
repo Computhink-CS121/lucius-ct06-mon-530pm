7b33060a1d2b4607b20c581f4b9ea8ff8774f712
Computhink-CS121 <cs121@computhink.com.sg>
2025-02-03 09:46:39 +0000

    Feb 3, 2025, 9:46 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -17,7 +17,7 @@
 #Assign a name to the variable "user_name"
 #Assign an integer to the variable "user_age"
 #Use type conversion and string concatenation to print out
-#
+#"{}"
 # year_born = input("what year are you born")
 # year_born = int(year_born)
 # current_year = 2025
