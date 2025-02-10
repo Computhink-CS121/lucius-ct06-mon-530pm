0043393eea3b2d51fff384a291af71ec34a3d2a3
Computhink-CS121 <cs121@computhink.com.sg>
2025-02-10 10:15:24 +0000

    Feb 10, 2025, 10:15 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -6,15 +6,32 @@ for number in range(51, 101):
     print(number)
       
 
-for  in range(18, 30):
+# for  in range(18, 30):
+#     print(number)
+
+
+for number in range(1, 5, 1): 
     print(number)
 
 
-
+for number in range(51, 101, 1): 
     print(number)
 
+## Task 5: range(start, stop)
+
+# **Task 5a**:
+# Print numbers from 1 to 5 using a 'for' loop.
 
 
+
+# **Task 5b**:
+# Print numbers from 51 to 100 using a 'for' loop.
+
+
+
+
+# **Task 5c**:
+# Print numbers from 18 to 29 using a 'for' loop.
 
 
 
