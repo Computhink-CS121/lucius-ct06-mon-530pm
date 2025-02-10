32a897357ae8d8be3e3dfdb7a42358f2e845fdc0
Computhink-CS121 <cs121@computhink.com.sg>
2025-02-10 10:09:17 +0000

    Feb 10, 2025, 10:09 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,10 +1,12 @@
 print("Hello from lesson 5")
 for number in range(1, 6): print(1,2,3,4,5)
 
-for number in range(51, 101): print(1,2,3,4,5)
+for number in range(51, 101): 
+    print(number )
+      
 
-
-
+for 30 in range(18, 30):
+    print(number )
 
 
 
