ca15513f15b3450bd216b6de7b0c5b564351a792
Computhink-CS121 <cs121@computhink.com.sg>
2025-02-24 10:33:05 +0000

    Feb 24, 2025, 10:33 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -33,7 +33,7 @@ input("What word would you like to repeat?") ("burger")
 input("How many times would you like to repeat?") ("3")
 
 word = input("give me a word")
-number_n = int()
+number_n = int(input("give me a number"))
 
 
 
