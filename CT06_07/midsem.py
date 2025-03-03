7fb29214e04874f99a1fd921497e0e578bd5305e
Computhink-CS121 <cs121@computhink.com.sg>
2025-03-03 10:14:50 +0000

    Mar 3, 2025, 10:14 AM

--- CT06_07/midsem.py
+++ CT06_07/midsem.py
@@ -1,6 +1,6 @@
 
-name = input("what is your name?")  
-print("Nice to meet you, Andy!")  
+# name = input("what is your name?")  
+# print("Nice to meet you, Andy!")  
 
 #i am trying to create a code that asks the user what is their name and then print nice to meet you person's name
 
