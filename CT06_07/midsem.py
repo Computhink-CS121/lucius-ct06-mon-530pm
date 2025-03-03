803eea26469c5453944fafaebe6b08a853e7b5cd
Computhink-CS121 <cs121@computhink.com.sg>
2025-03-03 10:28:01 +0000

    Mar 3, 2025, 10:28 AM

--- CT06_07/midsem.py
+++ CT06_07/midsem.py
@@ -30,10 +30,10 @@ for i in range (7,0,-3):
 
 
 
-name = input("what is the start, end, and `increment")
+name = input("what is the start, end, and increment")
 for i in range (0,78,7):
     print(i)
-#i am trying to create a code that asks a user when he wants 
+#i am trying to create a code that asks a user when he wants he start, end, and increment
 
 
 
