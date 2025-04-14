2209ec28b304ff52e73e4e18f0afced028eae09b
Computhink-CS121 <cs121@computhink.com.sg>
2025-04-14 09:51:49 +0000

    Apr 14, 2025, 9:51 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,7 +1,7 @@
 print("Hello from lesson 11")
 
 colour = input("tell me your favourite colour")
-if not (colour == "Green"):
+if not (colour == "green"):
     print("try again")
 else:
     print("try again")
