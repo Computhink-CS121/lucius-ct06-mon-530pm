b0204f5bffd07b9a277a9921731a871b8e695424
Computhink-CS121 <cs121@computhink.com.sg>
2025-04-14 09:43:16 +0000

    Apr 14, 2025, 9:43 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,1 +1,244 @@
-print("Hello from lesson 9")
+input("how many days have you borrowed the book for?")
+answer = input
+answer = int(answer) 
+if answer == (more than 25)
+ 
+
+# #task 3a
+
+# import random
+# somenumber = random.randint(1,10)
+# answer = input("guess a number from 1 to 10: ")
+# answer = int(answer) # convert to number from string
+# if answer == somenumber:
+#     print("congratulations")
+
+#task 5
+numapples = int(input("how many apples? "))
+numoranges = int(input("how many oranges?"))
+if numapples > 4:
+    apples = numapples *0.6*0.9
+else:
+    apples = numapples *0.6
+if numoranges > 4: # decision
+    oranges = numoranges*0.9
+else:
+    oranges = numoranges *0.9*0.9
+
+total = apples + oranges
+print("please pay $" + str(total))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,47 @@
-print("Hello from lesson 10")
+# #recap 1
+
+# # import random
+# # somenumber = random.randint(1,15)
+# # answer = input("guess a number from 1 to 15: ")
+# # answer = int(answer) # convert to number from string
+# # if answer == somenumber:
+# #     print("thats it!")
+
+
+
+# answer = int(input("write a number:"))
+# if answer > -1:
+#     print(str(answer) + "is a positive number")
+# else:
+#     print( str(answer) + "is a negative number")
+
+
+
+import random
+somenumber = random.randint(1,15)
+answer = input("guess a number from 1 to 2: ")
+answer = int(answer) # convert to number from string
+if answer == somenumber:
+    print("Congratulations! You did it!")
+else:
+    print("Oops, better luck next time!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
