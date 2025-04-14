a4ae360d1f94426206c755a67a4902242f2dcbbc
Computhink-CS121 <cs121@computhink.com.sg>
2025-04-14 09:59:34 +0000

    Apr 14, 2025, 9:59 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -12,8 +12,8 @@ if not (time == "saturday"):
     print("its not the weekend yet!")
 else:
     print("hooray! its the weekend no school!")
-
-
+if not (time == "sunday"):
+    print("its not the weekend yet!")
 
 
 
