c6224bd471157fbf7c11f6eb5a4fee24e8fa29e9
Computhink-CS121 <cs121@computhink.com.sg>
2025-04-21 10:17:40 +0000

    Apr 21, 2025, 10:17 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -55,7 +55,7 @@ for count in range(5):
  number1 = random.randint(1,10)
 number2 = random.randint(1,10)
 answer = input("what is 5+3?")
-question = "what is " + str(number1) + "+" + str(number2) + "3"
+question = "what is " + str(number1) + "+" + str(number2) 
 answer = int(input(question))
 hidden_answer = number1 + number2
 while not answer == hidden_answer:
